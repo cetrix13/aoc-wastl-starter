0de1925d61bbf12da14eff19ce1696aa06c0a0a3
Alexandr Dadykin <DadykinAV@yandex.ru>
2018-12-05 23:02:35 +0300

    Add doc blocks and constant

--- src/day_1.php
+++ src/day_1.php
@@ -4,30 +4,38 @@ namespace AoC;
 class FrequencyCalc
 {
     private $list = [];
+    const FILE_PATH = __DIR__ . "/resourses/frequency_list.txt";
 
-    public function __construct($filePath)
+    public function __construct()
     {
-        $this->list = file($filePath, FILE_IGNORE_NEW_LINES) or die('File not found');
+        $this->list = file(self::FILE_PATH, FILE_IGNORE_NEW_LINES) or die('File not found');
     }
 
+    /**
+     * @return integer
+     */
     public function calculate()
     {
+        $sum = 0;
         foreach ($this->list as $item) {
             $sum += $item;
         }
         return $sum;
     }
 
+    /**
+     * @return integer
+     */
     public function reachedTwiceFrequency()
     {
-        $listUniqueFrequences = [0];
+        $uniqueFrequences = [0];
         $freqValue = 0;
 
         while (true) {
             foreach ($this->list as $item) {
                 $freqValue += $item;
-                if (!in_array($freqValue, $listUniqueFrequences)) {
-                    array_push($listUniqueFrequences, $freqValue);
+                if (!in_array($freqValue, $uniqueFrequences)) {
+                    array_push($uniqueFrequences, $freqValue);
                 } else {
                     return $freqValue;
                 }
@@ -36,6 +44,6 @@ class FrequencyCalc
     }
 }
 
-$obj = new FrequencyCalc(__DIR__ . "/resourses/frequency_list.txt");
+$obj = new FrequencyCalc();
 echo "Resulting frequency equals ". $obj->calculate(). "\n";
-echo "First frequency that reached twice is ". $obj->reachedTwiceFrequency() ."\n";
+echo "First frequency that was reached twice is ". $obj->reachedTwiceFrequency() ."\n";
