d14f8603fcfbdc02ceb1d8b03779129372d54221
Alexandr Dadykin <DadykinAV@yandex.ru>
2018-12-12 11:11:04 +0300

    Add solution for part 2 of the firth day

--- src/day_5.php
+++ src/day_5.php
@@ -13,9 +13,12 @@ class Polymer
         $this->polymerSequence = str_split(trim(file_get_contents(self::FILE_PATH)));
     }
 
+    /**
+     * @param  array $polymerSequence
+     * @return integer
+     */
     public function countPolymerUnits($polymerSequence)
     {
-
         do {
             $removed = false;
             foreach ($polymerSequence as $key => $current) {
@@ -28,9 +31,22 @@ class Polymer
             }
         } while ($removed);
 
-        return $polymerSequence;
+        return count($polymerSequence);
+    }
+
+    public function countPolymerUnitsWhenUnitTypeRemoved($type)
+    {
+        $copyPolymerSequence = $this->polymerSequence;
+        $copyPolymerSequence = str_split(str_replace($type, "", implode("", $copyPolymerSequence)));
+        $copyPolymerSequence = str_split(str_replace(strtolower($type), "", implode("", $copyPolymerSequence)));
+
+        return self::countPolymerUnits($copyPolymerSequence);
     }
 }
 
 $obj = new Polymer();
-echo "Number of units in shortest polymer = " . count($obj->countPolymerUnits($obj->polymerSequence)) . "\n";
+echo "Number of units in shortest polymer = " . $obj->countPolymerUnits($obj->polymerSequence) . "\n";
+echo "Remove units of type A and get polymer of lenght = " . $obj->countPolymerUnitsWhenUnitTypeRemoved("A") . "\n";
+echo "Remove units of type B and get polymer of lenght = " . $obj->countPolymerUnitsWhenUnitTypeRemoved("B") . "\n";
+echo "Remove units of type C and get polymer of lenght = " . $obj->countPolymerUnitsWhenUnitTypeRemoved("C") . "\n";
+echo "Remove units of type D and get polymer of lenght = " . $obj->countPolymerUnitsWhenUnitTypeRemoved("D") . "\n";
